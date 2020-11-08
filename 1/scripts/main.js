85c35f446d651d1c7138c3beaff492f52dd95263
Marjona Mirzoeva <mirzoeva.top@mail.ru>
2020-11-09 01:04:42 +0400

    Add adding to basket

--- 1/scripts/main.js
+++ 1/scripts/main.js
@@ -5,45 +5,77 @@ if (localStorage.getItem("countProducts") == null) {
     $('.js-product-count').text(countProducts);
 }
 
-localStorage.setItem("products", new Map([
+if (localStorage.getItem("basket") == null) {
+    let map = new Map();
+    localStorage.setItem("basket", JSON.stringify(Array.from(map.entries())));
+}
+
+let products = new Map([
     [1, {
+        img: '../img/socks_1.png',
         name: "Носки. Устрицы",
         price: 299
     }],
     [2, {
+        img: '../img/socks_2.png',
         name: "Носки. Мартин",
         price: 299
     }],
     [3, {
+        img: '../img/socks_3.png',
         name: "Носки. Кальмары",
         price: 299
     }],
     [4, {
+        img: '../img/socks_4.png',
         name: "Носки. Голуби",
         price: 299
     }],
     [5, {
+        img: '../img/socks_5.png',
         name: "Носки. Геометрия",
         price: 299
     }],
     [6, {
+        img: '../img/socks_6.png',
         name: "носки. Суши",
         price: 299
     }],
     [7, {
+        img: '../img/socks_7.png',
         name: "Носки. Моллюски",
         price: 299
     }],
     [8, {
+        img: '../img/socks_8.png',
         name: "Носки. Зебра",
         price: 299
     }]
-    ]));
+]);
+
+ localStorage.setItem("products", JSON.stringify(Array.from(products.entries())));
 
 $('.js-open-basket').click(function (event) {
     console.log("open basket");
     $('.js-basket').removeClass("js-hidden");
     $('.js-overlay').removeClass("js-hidden");
+
+    let basketJson = localStorage.getItem("basket");
+    let basketMap = new Map(JSON.parse(basketJson));
+    $('.js-basket-items').empty();
+
+    let pairs = Array.from(basketMap.entries());
+    for (let i = 0; i < pairs.length; i++) {
+        let pair = pairs[i];
+        let value = pair[1];
+
+        let productInfo = products.get(value.productId);
+        productInfo.count = value.quantity;
+        productInfo.size = value.size;
+
+        let row = createBasketRow(productInfo);
+        $('.js-basket-items').append(row);
+    }
 });
 
 $('.js-close-basket').click(function (event) {
@@ -70,15 +102,37 @@ $('.js-add-to-basket').click(function (event) {
         localStorage.setItem("countProducts", newCount);
         $('.js-product-count').text(newCount);
 
-        let row = document.querySelector('#productItemRow');
-        // $('.js-basket').appendChild(document.importNode(row.content, true));
-        $('.js-basket').appendChild("<span>123</span>");
-        // let productInfo = localStorage.getItem('products').get(productId);
-        // $('.js-basket').append(productItemRow.content.cloneNode(true));
         console.log(newCount);
+
+        let basketJson = localStorage.getItem("basket");
+        let basketMap = new Map(JSON.parse(basketJson));
+
+        let productId_size = productId + socksSize;
+        if (basketMap.has(productId_size)) {
+            let productInBasket = basketMap.get(productId_size);
+            productInBasket.quantity++;
+        } else {
+            basketMap.set(productId_size, {
+                productId: productId,
+                quantity: 1,
+                size: socksSize
+            })
+        }
+
+        localStorage.setItem("basket", JSON.stringify(Array.from(basketMap.entries())));
     }
 })
 
-$('.js-overlay').click(function (event){
+function createBasketRow (productInfo) {
+    let clone = productItemRow.content.cloneNode(true);
+    $(clone).find(".js-product-img").attr("src", productInfo.img);
+    $(clone).find(".js-product-name").text(productInfo.name);
+    $(clone).find(".js-product-size").text(productInfo.size);
+    $(clone).find(".js-product-quantity").text(productInfo.count);
+    $(clone).find(".js-product-price").text(productInfo.count*productInfo.price);
+    return clone;
+}
+
+$('.js-overlay').click(function (event) {
     $('.js-basket, .js-notification, .js-overlay').addClass("js-hidden");
 })
